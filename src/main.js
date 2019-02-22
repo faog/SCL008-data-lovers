2de898c2983752297bb036fa0867b772d4964fbd
faog <fabiola.orellana.g@gmail.com>
2019-02-22 09:39:58 -0300

    Dom para barra nav para pantalla responsiva

--- src/main.js
+++ src/main.js
@@ -1,32 +1,56 @@
 /* Manejo del DOM */
-
 document.getElementsByTagName('a')[0].addEventListener('click', () => {
     document.getElementById('dinamicpage').innerHTML = '';
     document.getElementById('dinamicpage').innerHTML += 
-    `<section>
-      <img src="Image/welcomeimage.jpg" id="welcomeimage" alt="Imagen de bienvenida">
-        <h2>¿QUIERES SER EL MEJOR MAESTRO POKÉMON?</h2>
-        <h3>Junto a PokeChoose podrás conseguirlo</h3>
+    `<section id="welcomeimage">
+        <h2 class="center-align">¿QUIÉRES SER EL MEJOR MAESTRO POKÉMON?</h2>          
+        <h3 class="center-align">Junto a PokeChoose podrás conseguirlo.</h3>
+        <div class="center-align"> 
         <button id="start">Comenzar</button>
-    </section>
+        </div>     
+    </section>      
+        <h3>Curiosidades</h3>
     <section>
-        <h3>Estadísticas</h3>
+        <h4>Estadistica</h4>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col l4 m6 s12">1</div>
+                <div class="col l4 m6 s12">2</div>
+                <div class="col l4 m6 s12">3</div>
+                <div class="col l4 m6 s12">4</div>
+                <div class="col l4 m6 s12">5</div>
+                <div class="col l4 m6 s12">6</div>
+            </div>
+        </div>
     </section>`
-
+    document.getElementsByTagName('button')[0].addEventListener('click', () =>{
+        document.getElementById('dinamicpage').innerHTML='';
+        document.getElementById('dinamicpage').innerHTML+= `<aside> <h6> Aquí va el filtro </h6> </aside>`; 
+    })
 })
 
+Array.from(document.getElementsByClassName('tutorial')).forEach(element => {
+    element.addEventListener('click', () =>{
+        document.getElementById('dinamicpage').innerHTML='';
+        document.getElementById('dinamicpage').innerHTML+= `<section><p>h</p><h1> Como funciona </h1> </section>`; 
+    })
+});
 
-document.getElementsByTagName('li')[0].addEventListener('click', () =>{
-    document.getElementById('dinamicpage').innerHTML='';
-    document.getElementById('dinamicpage').innerHTML+= `<section><h3> Como funciona </h3> </section>`; 
-})
-
-document.getElementsByTagName('li')[1].addEventListener('click', () =>{
-    document.getElementById('dinamicpage').innerHTML='';
-    document.getElementById('dinamicpage').innerHTML+= `<aside> <h3> Aquí va el filtro </h3> </aside>`; 
-})
+Array.from(document.getElementsByClassName('search')).forEach(element => {
+    element.addEventListener('click', () =>{
+        document.getElementById('dinamicpage').innerHTML='';
+        document.getElementById('dinamicpage').innerHTML+= `<aside><p>h</p> <h1> Iniciar búsqueda </h1> </aside>`; 
+    })
+});
 
 document.getElementsByTagName('button')[0].addEventListener('click', () =>{
     document.getElementById('dinamicpage').innerHTML='';
-    document.getElementById('dinamicpage').innerHTML+= `<aside> <h3> Aquí va el filtro </h3> </aside>`; 
+    document.getElementById('dinamicpage').innerHTML+= `<aside><p>h</p> <h1> Iniciar búsqueda </h1> </aside>`; 
 })
+
+
+//funciones complementarias a MaterializeCSS
+document.addEventListener('DOMContentLoaded', () =>{
+    let elems = document.querySelectorAll('.sidenav');
+    let instances = M.Sidenav.init(elems);
+});
