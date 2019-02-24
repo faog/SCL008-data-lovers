87fc81fdc6b002a19c73c290831881e11ed775e7
faog <fabiola.orellana.g@gmail.com>
2019-02-24 18:03:54 -0300

    Actualización manejo del DOM (DRY)

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-/* Manejo del DOM */
-document.getElementsByTagName('a')[0].addEventListener('click', () => {
+/* global M*/
+
+/*I.Declaración de vistas*/
+/*a)Pagina de inicio*/
+function indexView(){
     document.getElementById('dinamicpage').innerHTML = '';
     document.getElementById('dinamicpage').innerHTML += 
     `<section id="welcomeimage">
         <h2 class="center-align">¿QUIÉRES SER EL MEJOR MAESTRO POKÉMON?</h2>          
         <h3 class="center-align">Junto a PokeChoose podrás conseguirlo.</h3>
         <div class="center-align"> 
-            <button id="start">Comenzar</button>
+            <button id="btnstart">Comenzar</button>
         </div>     
     </section>   
     <h3>Estadistica</h3>
@@ -30,28 +33,111 @@ document.getElementsByTagName('a')[0].addEventListener('click', () => {
         </div>        
     </section>`
     document.getElementsByTagName('button')[0].addEventListener('click', () =>{
-        document.getElementById('dinamicpage').innerHTML='';
-        document.getElementById('dinamicpage').innerHTML+= `<aside> <h6> Aquí va el filtro </h6> </aside>`; 
+        searchView(); 
     })
+    let elemsCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elemsCarousel);
+
+}
+
+function tutorialView(){
+    document.getElementById('dinamicpage').innerHTML = '';
+    document.getElementById('dinamicpage').innerHTML += 
+    `<section >
+        <h4>breve introducción de que es PokeChoose, indicando que son 151 pokemon de kanto (quizás agregar un mapa)</h1>
+        <article>
+        <h4>explicar las distintas funcionalidades de la página</h1>
+        </article>
+    </section>`
+}
+
+
+function searchView(){
+    document.getElementById('dinamicpage').innerHTML = '';
+    document.getElementById('dinamicpage').innerHTML += 
+    `
+    <section class="row">     
+    <div class="col s12 m12 l3 fullheight">
+      <ul class="collapsible">
+        <li>
+          <div class="collapsible-header">Filtrar</div>
+          <div class="collapsible-body">
+            <label>Tipo</label>
+              <select class="browser-default">
+                <option actived>Todos</option>                
+                <option>Agua</option>
+                <option>Fuego</option>
+                <option>Planta</option>
+               </select>   
+            <label>Debilidad</label>
+            <select class="browser-default">
+                <option actived>Todos</option>                
+                <option>Agua</option>
+                <option>Fuego</option>
+                <option>Planta</option>
+            </select> 
+            <button id="btnfilter">Filtar</button>          
+          </div>
+        </li>
+        <li>
+          <div class="collapsible-header">Ordenar</div>
+          <div class="collapsible-body">
+          <label>Nombre</label>
+          <select class="browser-default">
+            <option actived>Escoja una opcion</option>                
+            <option>A-Z</option>
+            <option>Z-A</option>            
+           </select>   
+        <button id="btnshort">Ordenar</button>   
+          </div>
+        </li>
+      </ul>       
+    </div>           
+    
+    <section id="pokemonresult" class="col s12 m12 l9" >
+      <h1>Resultado pokemon</h1>
+      <h1>Resultado pokemon</h1>
+      <h1>Resultado pokemon</h1>
+      <h1>Resultado pokemon</h1>
+      <h1>Resultado pokemon</h1>
+      <h1>Resultado pokemon</h1>
+      <h1>Resultado pokemon</h1>
+      <h1>Resultado pokemon</h1>
+      <h1>Resultado pokemon</h1>
+      <h1>Resultado pokemon</h1>
+      <h1>Resultado pokemon</h1>
+      <h1>Resultado pokemon</h1>
+      <h1>Resultado pokemon</h1>
+      <h1>Resultado pokemon</h1>    
+    </section>
+
+  </section>  
+  `
+  let elems = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(elems);
+
+  let elemsSelect = document.querySelectorAll('select');
+  M.FormSelect.init(elemsSelect);
+}
+
+
+/*II.Manejo del DOM */
+/*a)Página de inicio*/
+document.getElementsByTagName('a')[0].addEventListener('click', () => {
+    indexView();
 })
 
 Array.from(document.getElementsByClassName('tutorial')).forEach(element => {
     element.addEventListener('click', () =>{
-        document.getElementById('dinamicpage').innerHTML='';
-        document.getElementById('dinamicpage').innerHTML+= `<section><p>h</p><h1> Como funciona </h1> </section>`; 
+        tutorialView();
     })
 });
 
 Array.from(document.getElementsByClassName('search')).forEach(element => {
     element.addEventListener('click', () =>{
-        document.getElementById('dinamicpage').innerHTML='';
-        document.getElementById('dinamicpage').innerHTML+= `<aside><p>h</p> <h1> Iniciar búsqueda </h1> </aside>`; 
+        searchView();
     })
 });
 
-document.getElementsByTagName('button')[0].addEventListener('click', () =>{
-    document.getElementById('dinamicpage').innerHTML='';
-    document.getElementById('dinamicpage').innerHTML+= `<aside><p>h</p> <h1> Iniciar búsqueda </h1> </aside>`; 
-})
 
 
